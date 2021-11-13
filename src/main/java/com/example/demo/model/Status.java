ad32657fad7bab62295b61f25a33c74183989801
VijayTamil1910 <vijaytamilarasan1910@gmail.com>
2021-11-13 21:42:00 +0530

    All files upload

---
+++ src/main/java/com/example/demo/model/Status.java
@@ -0,0 +1,33 @@
+package com.example.demo.model;
+
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.example.demo.utility.Timestamp;
+
+@Document(collection = "StatusHistory")
+public class Status {
+	private String defectId;
+	private Timestamp dateTime;
+	private String status;
+	
+	public String getDefectId() {
+		return defectId;
+	}
+	public void setDefectId(String defectId) {
+		this.defectId = defectId;
+	}
+	public Timestamp getDateTime() {
+		return dateTime;
+	}
+	public void setDateTime(Timestamp dateTime) {
+		this.dateTime = dateTime;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	
+	
+}
---
+++ src/main/java/com/example/demo/utility/Dashboard.java
@@ -0,0 +1,35 @@
+package com.example.demo.utility;
+
+import java.util.List;
+
+import com.example.demo.model.Comments;
+import com.example.demo.model.Defect;
+import com.example.demo.model.Status;
+
+public class Dashboard {
+	private Defect defect;
+	private List<Status> status;
+	private List<Comments> comments;
+	
+	public List<Comments> getComments() {
+		return comments;
+	}
+	public void setComments(List<Comments> comments) {
+		this.comments = comments;
+	}
+	public Defect getDefect() {
+		return defect;
+	}
+	public void setDefect(Defect defect) {
+		this.defect = defect;
+	}
+	public List<Status> getStatus() {
+		return status;
+	}
+	public void setStatus(List<Status> status) {
+		this.status = status;
+	}
+	
+	
+	
+}
